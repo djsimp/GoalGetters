96103e97b3c8b4d9655d313bae07d00b075a564c
djsimp <dougjr.simpson@gmail.com>
2017-09-27 23:46:45 -0600

    Initial Commit #2

---
+++ AndroidStudioProjects/GoalGetters/app/src/main/java/org/truthdefender/goalgetters/MainActivity.java
@@ -0,0 +1,13 @@
+package org.truthdefender.goalgetters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
